af7f26fd4c0c57db97eff3f468caa72c8321d32f
mhpark135 <mhpark@ucdavis.edu>
2023-05-02 13:14:10 -0700

    d

---
+++ check/script.js
@@ -0,0 +1,64 @@
+let globalData;
+async function getData(){
+    const myMoods = await fetch ('data/mood.json');
+    const data = await myMoods.json();
+    globalData = data;
+    // console.log(data);
+    // document.querySelector('#moods').innerHTML = outputHTML(data);
+    document.querySelector('nav ul').innerHTML = createButtons(data);
+
+    createEvents();
+}
+
+function outputHTML(data){
+    const feeling = ['really bad', 'grumpy', 'meh', 'ok', 'good!', 'wicked awesome!'];
+    let html = '<p>';
+    html += `At ${data.point2.time} I was feeling ${feeling[data.point2.mood]} because of ${data.point2.reason}`;
+    html += '<p>';
+    return html;
+}
+
+function createButtons(data){
+    let html = ''
+    // convert the main object keys into an array
+    const dataPoints = Object.keys(data);
+    console.log(dataPoints);
+    dataPoints.forEach(function(eachPoint){
+        html +=  `<li><button id="${eachPoint}">${eachPoint}</button></li>`;    
+    })
+    return html;
+}
+
+function createEvents(){
+    const buttons = document.querySelectorAll('button');
+    //console.log(buttons) 
+
+    for (const button of buttons){
+        button.addEventListener('click', function(event){
+            const id = event.target.id; 
+            // console.log(id)
+            updateInterface(id, globalData);
+        })
+    }
+}
+
+function updateInterface(value, jsonData){
+    console.log(value);
+    const feeling = ['really bad', 'grumpy', 'meh', 'ok', 'good', 'wicked awesome!'];
+    let text = '<p>';
+    let imgs = '';
+    text += `At ${jsonData[value].time} I was feeling ${feeling[jsonData[value].mood]} because of ${jsonData[value].reason}`;
+    text += '</p>';
+    if (jsonData[value].hasOwnProperty('images')){
+        for (let i=0; i<jsonData[value].images.length; i++ ){
+            console.log(jsonData[value].images[i]);
+                imgs += `<img src="${jsonData[value].images[i]}"  alt="image">`
+        }
+    } else {
+        imgs = '';
+    }
+    document.querySelector('#result').innerHTML = text;
+    document.querySelector('#images').innerHTML = imgs;
+    
+}
+getData();
