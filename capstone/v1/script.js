f5c6d20690e989fd67b34116238fa43eecad281b
mhpark135 <mhpark@ucdavis.edu>
2023-05-25 13:50:00 -0700

    d

--- capstone/v1/script.js
+++ capstone/v1/script.js
@@ -1,12 +1,17 @@
 (function() {
     const buttonGallery = document.getElementById('buttonGallery');
     const buttonInstructions = document.getElementById('buttonInstructions');
-    const gallery = document.querySelector('#gallery')
-    const instructions = document.querySelector('#instructions')
-
+    const gallery = document.querySelector('#gallery');
+    const instructions = document.querySelector('#instructions');
+    const close = document.querySelector('#buttonClose');
 
     buttonGallery.addEventListener('click', function() {
         gallery.className = 'showing';
     })
+
+    close.addEventListener('click', function() {
+        gallery.className = 'hidden';
+    })
+
 
 })();
